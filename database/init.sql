0eeacfe5a53bb96b124ac4c3cb73c13dda89f377
Leemainsw <s2018s15@e-mirim.hs.kr>
2020-10-29 13:27:37 +0900

    1027 작업

--- database/init.sql
+++ database/init.sql
@@ -1,18 +1,52 @@
-CREATE DATABASE AH_3214;
 
-CREATE TABLE user(
-    user_idX VARCHAR(45) AUTO_INCREMENT NOT NULL,
-    user_id VARCHAR(45) NOT NULL,
+-- USER SQL
+CREATE USER "3214DBPROJECT" IDENTIFIED BY "1234"  
+DEFAULT TABLESPACE "USERS"
+TEMPORARY TABLESPACE "TEMP";
+
+-- QUOTAS
+ALTER USER "3214DBPROJECT" QUOTA 10M ON "USERS";
+
+-- ROLES
+GRANT "DBA" TO "3214DBPROJECT" ;
+GRANT "CONNECT" TO "3214DBPROJECT" ;
+GRANT "RESOURCE" TO "3214DBPROJECT" ;
+
+-- SYSTEM PRIVILEGES
+
+CREATE TABLE users(
+    user_idx int NOT NULL,
+    user_id VARCHAR(40) NOT NULL,
     user_password VARCHAR(255) NOT NULL,
-    user_name VARCHAR(20) NOT NULL,
-    user_address VARCHAR(50) NOT NULL
+    PRIMARY KEY(user_idx)
 );
 
-CREATE TABLE dog(
-    dog_id VARCHAR(45) NOT NULL,
-    user_idx VARCHAR(45) NOT NULL,
-    dog_name VARCHAR2(20) NOT NULL,
-    dog_birth DATETIME NOT NULL,
-    dog_idx VARCHAR(15) NOT NULL,
-    PRIMARY KEY(dog_idx);
+CREATE TABLE DOG(
+    dog_idx int NOT NULL,
+    dog_name VARCHAR(45) NOT NULL,
+    dog_birth DATE NOT NULL,
+    user_idx int NOT NULL,
+    PRIMARY KEY(dog_idx)
 );
+
+CREATE TABLE HOSPITAL(
+    hospital_idx int NOT NULL,
+    hospital_name VARCHAR(50) NOT NULL,
+    hospital_address VARCHAR(50) NOT NULL,
+    hospital_rate VARCHAR(5) NOT NULL,
+    PRIMARY KEY(hospital_idx)
+);
+
+CREATE TABLE REVIEW(
+    review_idx int NOT NULL,
+    hospital_idx int NOT NULL,
+    user_idx int NOT NULL,
+    review_rate VARCHAR(5) NOT NULL,
+    review_title VARCHAR(50) NOT NULL,
+    review_body VARCHAR(500) NOT NULL,
+    review_date DATE NOT NULL,
+    PRIMARY KEY(review_idx)
+);
+
+
+CREATE SEQUENCE IDX_INCREMENT INCREMENT BY 1 START WITH 1 MINVALUE 1;
---
+++ model/database.js
@@ -0,0 +1,8 @@
+// school
+module.exports={
+    user: '3214DBPROJECT',
+    password: '1234',
+    host: 'localhost', 
+    database: 'xe',
+    port: '1521'
+};
